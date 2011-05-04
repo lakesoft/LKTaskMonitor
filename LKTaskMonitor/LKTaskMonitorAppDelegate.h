d9f246b0c87bed10b3585d5f9c8e2515960b7a21
Hiroshi Hashiguchi <xcatsan@mac.com>
2011-05-04 23:26:19 +0900

    Initial Commit

---
+++ LKTaskMonitor/LKTaskMonitorAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  LKTaskMonitorAppDelegate.h
+//  LKTaskMonitor
+//
+//  Created by Hiroshi Hashiguchi on 11/05/04.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LKTaskMonitorAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
